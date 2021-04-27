c0508db60655a981b0e89ebc28467996716ae22d
bestoon <bestoonkhurshid@hotmail.com>
2021-04-28 01:12:03 +0200

    initial commit

--- portscanner.py
+++ portscanner.py
@@ -1,12 +1,15 @@
+
 import argparse
 import socket
+import time
 
 parser = argparse.ArgumentParser(description='Port scanner')
 parser.add_argument('-t', '--target', type=str, help='target ip')
 
 arge = parser.parse_args()
+start = time.time()
 
-for port in range(1, 1000):
+for port in range(1, 65000):
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.settimeout(0.1)
 	result = s.connect_ex((arge.target, port))
@@ -14,5 +17,6 @@ for port in range(1, 1000):
 		print(f' port {port} is open')
 	else:
 		pass
+end = time.time()
 
-
+print(f'Total scan time in second: {int(end - start)}')
