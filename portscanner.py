463f703c013d864f3301d03d9346fcb4c24eb24e
Bestoonit <57560613+Bestoonit@users.noreply.github.com>
2021-07-22 16:44:43 +0200

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,25 +1,29 @@
-#some Module importation
 import argparse
 import socket
 import time
+import concurrent.futures
 
-#creating parser object
 parser = argparse.ArgumentParser(description='Port scanner')
 
-#adding argument as parameter 
-parser.add_argument('-t', '--target', type=str, help='target ip')
-
-arge = parser.parse_args()
+parser.add_argument('-t', '--target', type=str)
+parser.add_argument('-p', '--number_of_ports', type=int)
+args = parser.parse_args()
 start = time.time()
+range_of_ports = list(range(1, args.number_of_ports))
 
-for port in range(1, 1000):
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.settimeout(0.1)
-	result = s.connect_ex((arge.target, port))
-	if result == 0:
-		print(f' port {port} is open')
-	else:
-		pass
+def portscanner(*range_of_ports):
+    for port in range_of_ports:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.1)
+        result = s.connect_ex((args.target, port))
+        if result == 0:
+            print(f' port {port} is open')
+        else:
+            pass
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(portscanner, range_of_ports)
+
 end = time.time()
 
 print(f'Total scan time in second: {int(end - start)}')
