e618ee17422f39dc7dbcf6a8633d4055dbbf9e8f
Bestoonit <57560613+Bestoonit@users.noreply.github.com>
2021-07-29 14:59:21 +0200

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,29 +1,44 @@
 import argparse
 import socket
-import time
+import time, datetime
 import concurrent.futures
+import pyfiglet
+
+print(pyfiglet.figlet_format("Port scanner"))
+print('-'*70)
 
 parser = argparse.ArgumentParser(description='Port scanner')
 
 parser.add_argument('-t', '--target', type=str)
 parser.add_argument('-p', '--number_of_ports', type=int)
 args = parser.parse_args()
+
+fqd = socket.getfqdn(args.target)
+print(f' FQDN > {fqd}  :  IP > {args.target}')
+print('-'*70)
+print('Open Ports')
+print('- '* 7)
 start = time.time()
 range_of_ports = list(range(1, args.number_of_ports))
-
 def portscanner(*range_of_ports):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     for port in range_of_ports:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(0.1)
         result = s.connect_ex((args.target, port))
-        if result == 0:
-            print(f' port {port} is open')
-        else:
-            pass
-
+        try:
+            if result == 0:
+                print(port, socket.getservbyport(port))
+        except socket.error:
+            print(port)
+            continue
+      
 with concurrent.futures.ThreadPoolExecutor() as executor:
     executor.map(portscanner, range_of_ports)
 
 end = time.time()
+time_took = str(end-start)
+x = slice(0, 4)
 
-print(f'Total scan time in second: {int(end - start)}')
+print(f'\nTime took in second: {time_took[x]}')
+print('-'*70)
+print('<', datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), '>\n')
